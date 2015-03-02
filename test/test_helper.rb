cd55639fd33132ad14fd6ebc9e16345fdacd148f
Lucas Allan Amorim <lucas.allan@gmail.com>
2015-03-02 16:28:23 -0300

    Moving mock.rb to test/ directory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ Coveralls.wear!
 require 'test/unit'
 
 require 'ref'
-require 'ref/mock'
+require 'mock'
 
 require_relative './reference_key_map_behavior'
 require_relative './reference_value_map_behavior'
